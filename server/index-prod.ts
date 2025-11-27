c67353eed4af76567b2f918678c51bf19c09b00f
Yacine Djeddi <yacine.djeddi.95@gmail.com>
2025-11-26 20:03:59 -0800

    Update index-prod.ts

--- server/index-prod.ts
+++ server/index-prod.ts
@@ -16,7 +16,7 @@ export async function serveStatic(app: Express, _server: Server) {
 
   app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist
+
   app.use("*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
