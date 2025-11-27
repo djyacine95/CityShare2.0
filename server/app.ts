0f94435452c84d2cca4a054597df1df2a3a9a487
Yacine Djeddi <yacine.djeddi.95@gmail.com>
2025-11-26 20:02:56 -0800

    Clean up comments in app.ts
    Removed unnecessary comments regarding route setup and port serving.

--- server/app.ts
+++ server/app.ts
@@ -77,14 +77,10 @@ export default async function runApp(
     throw err;
   });
 
-  // importantly run the final setup after setting up all the other routes so
-  // the catch-all route doesn't interfere with the other routes
+
   await setup(app, server);
 
-  // ALWAYS serve the app on the port specified in the environment variable PORT
-  // Other ports are firewalled. Default to 5000 if not specified.
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
+ 
   const port = parseInt(process.env.PORT || '5000', 10);
   server.listen({
     port,
