f29188d69c0d7b30fdb32d6bb6f1952472f9d988
Yacine Djeddi <yacine.djeddi.95@gmail.com>
2025-11-26 20:09:13 -0800

    Update database credentials in drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   schema: "./shared/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    // PASTE YOUR REAL NEON URL INSIDE THE QUOTES BELOW:
+   
     url: "postgresql://neondb_owner:npg_FiXwcx25Abfj@ep-round-bonus-ad8c2m9m-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
   },
 });
